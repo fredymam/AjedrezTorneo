c6a0257b7707f877fc40d1c350a2ba3fbb45e571
Marcelo Mana <fredymam@gmail.com>
2016-09-27 18:19:56 -0300

    Paquetes de organizacion y Clase Jugador (inicial)

---
+++ AjedrezTorneo/src/app/TestClases.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.sql.Date;
+
+import logica.Jugador;
+
+public class TestClases {
+
+	public static void main(String[] args) {
+		Jugador jugador = new Jugador();
+		System.out.println(jugador.getEdad());
+
+	}
+
+}
---
+++ AjedrezTorneo/src/logica/Jugador.java
@@ -0,0 +1,27 @@
+package logica;
+
+import java.util.*;
+
+public class Jugador {
+	private char sexo;
+	private Date nacimiento;
+	public Jugador() {
+		
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public Date getNacimiento() {
+		return nacimiento;
+	}
+	public void setNacimiento(Date nacimiento) {
+		this.nacimiento = nacimiento;
+	}
+	public int getEdad() {
+		
+	}
+	
+}
