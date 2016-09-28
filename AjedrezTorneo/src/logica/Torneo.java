dc7437f6360b0f7b65e684dd43cbacccf1850100
Julian Sosa <juliisosa73@gmail.com>
2016-09-28 20:22:50 -0300

    Creamos la clase Torneo, falta trabajar, no se termina mas el año la concha de la lora

---
+++ AjedrezTorneo/src/logica/Torneo.java
@@ -0,0 +1,15 @@
+package logica;
+import java.util.*;
+
+public class Torneo {
+	public String Lugar;
+	public Jugador[] Participantes;
+    public Arbitro[] Arbitros; 
+    public Ronda[] Rondas; 
+    public String Premio;
+    public Date Fecha;
+    public Jugabilidad[] Modo;
+    public Lugar[] Ubicacion;
+    public Desempate[] TieBreak;
+    
+}
