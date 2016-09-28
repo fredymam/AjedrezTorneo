9ffd0f6842ef5fd60cd1b9dbdea886926a4dae81
maxi perez <maxigperez@gmail.com>
2016-09-28 20:35:54 -0300

    agregue categorias

--- AjedrezTorneo/src/logica/Arbitro.java
+++ AjedrezTorneo/src/logica/Arbitro.java
@@ -16,5 +16,19 @@ public class Arbitro {
 		this.categoria = categoria;
 	}
 	
+	public void arbitro(){
+		switch (categoria){
+			case 0:
+			    System.out.println("arbitro mundial"); break;
+			case 1:
+				System.out.println("arbitro continental"); break;
+			case 2:
+				System.out.println("arbitro regional"); break;
+			case 3:
+				System.out.println("arbitro inactivo"); break;
+				
+			
+		}
+	}
 	
 }
