8dbf71c86e99257d1f2811934e4a8100a2b4d769
javier <Alumno@WIN8-21>
2016-10-05 20:26:22 -0300

    partida

---
+++ AjedrezTorneo/src/logica/Partida.java
@@ -0,0 +1,14 @@
+package logica;
+
+
+public class Partida {
+	private int Mesa;
+	private enum Resultado {BLANCAS, NEGRAS, NULL, UNDEFINE};
+	private Resultado[] resultado;
+	
+	public void partida(){
+		
+	}
+	
+
+}
