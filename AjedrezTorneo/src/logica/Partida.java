cf5f283f170e34795854121295d6721f3f4a5afb
javier <Alumno@WIN8-21>
2016-10-12 20:24:15 -0300

    partida

--- AjedrezTorneo/src/logica/Partida.java
+++ AjedrezTorneo/src/logica/Partida.java
@@ -1,28 +1,31 @@
 package logica;
 
-
 public class Partida {
 	private int Mesa;
 	private enum Resultado {BLANCAS, NEGRAS, NULL, UNDEFINE};
 	private Resultado resultado;
 	private Jugador jblancas;
 	private Jugador jnegras;
-	
+		
 	public void Partida(int Mesa, Jugador jblancas, Jugador jnegras){
 		this.Mesa = Mesa;
 		this.jblancas= jblancas;
 		this.jnegras= jnegras;
 	    resultado=Resultado.UNDEFINE;
 	}
-	public void setResultado (int Mesa, Resultado resultado){
+	public void setResultado (Resultado resultado){
 		if (resultado == Resultado.BLANCAS){
 			jblancas.score++;
 		}
 		if (resultado == Resultado.NEGRAS){
 			jnegras.score++;
 		}
-		if(resultado == Resultado.NULL)
+		if(resultado == Resultado.NULL){
+			jnegras.score = jnegras.score + 0.5;
+			jblancas.score = jblancas.score + 0.5;
+		}
 	}
 	
-
+  
+    
 }
