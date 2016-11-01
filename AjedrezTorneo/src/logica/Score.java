d4d9d5e5e4123da0e748eb91f5095dbc47870fbe
Alumno <Alumno@WIN8-05>
2016-11-01 19:10:49 -0300

    Agrege clase Score

---
+++ AjedrezTorneo/src/logica/Score.java
@@ -0,0 +1,69 @@
+package logica;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class Score extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table_score;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Score frame = new Score();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Score() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		
+		table_score = new JTable();
+		table_score.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"      Posicion ", "      Nombre ", "      Puntos"},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				"Posicion", "Nombre", "Puntos"
+			}
+		));
+		panel.add(table_score);
+	}
+
+}
