16803d51ef109e795b39fcee7daf97babd63aaff
shuliann <juliisosa73@gmail.com>
2016-11-09 20:28:52 -0300

    Continuacion de interfaz torneo

--- AjedrezTorneo/src/interfaz/InterfazTorneo.java
+++ AjedrezTorneo/src/interfaz/InterfazTorneo.java
@@ -37,6 +37,13 @@ public class InterfazTorneo extends JFrame {
 	private JComboBox<String> comboBox_Modalidad;
 	private JLabel lblNewLabel_1;
 	private JTextField textField;
+	private JPanel panel_desempate;
+	private JLabel lblDesempate;
+	private JComboBox comboBox_des2;
+	private JComboBox comboBox_des1;
+	private JComboBox comboBox_des3;
+	private JLabel lblNewLabel_2;
+	private JLabel label;
 
 	private void CrearTorneo(){
 		tournament.setNombre(textField_Nombre.getText());
@@ -47,6 +54,17 @@ public class InterfazTorneo extends JFrame {
 		} else { 
 		   tournament.setModalidad(Torneo.Modalidad.SWISS);
 		}
+		 /*if (comboBox_des1.getSelectedIndex!=0) {
+			switch (comboBox_des1.getSelectedIndex)
+				case 1 : ;
+				         break;
+				case 2 : sad;
+				         break;
+				case 3 :         
+			      
+			
+		} */
+		
 		// completar todos los datos
 		JOptionPane.showMessageDialog(null, "Torneo Registrado!");
 		setVisible(false); // Hace ventana invisible
@@ -58,12 +76,12 @@ public class InterfazTorneo extends JFrame {
 		setTitle("Ajedrez Torneo");
 		tournament = torneo;
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 589, 402);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 191, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(3, 1, 0, 0));
+		contentPane.setLayout(new GridLayout(4, 1, 0, 0));
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(192, 192, 192));
@@ -128,6 +146,36 @@ public class InterfazTorneo extends JFrame {
 		textField = new JTextField();
 		panel_2.add(textField);
 		textField.setColumns(10);
+		
+		panel_desempate = new JPanel();
+		panel_desempate.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		contentPane.add(panel_desempate);
+		panel_desempate.setLayout(new GridLayout(3, 2, 0, 0));
+		
+		lblNewLabel_2 = new JLabel("");
+		panel_desempate.add(lblNewLabel_2);
+		
+		comboBox_des1 = new JComboBox();
+		comboBox_des1.setModel(new DefaultComboBoxModel(new String[] {"", "1\u00BA Progresivo", "1\u00BA Bucholz", "1\u00BA SonnebornBerger"}));
+		panel_desempate.add(comboBox_des1);
+		
+		lblDesempate = new JLabel("Desempate");
+		lblDesempate.setBackground(new Color(0, 0, 0));
+		lblDesempate.setForeground(new Color(100, 149, 237));
+		lblDesempate.setFont(new Font("Segoe UI Black", Font.PLAIN, 14));
+		lblDesempate.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_desempate.add(lblDesempate);
+		
+		comboBox_des2 = new JComboBox();
+		comboBox_des2.setModel(new DefaultComboBoxModel(new String[] {"", "2\u00BA Progresivo", "2\u00BA Bucholz", "2\u00BA SonnebornBerger"}));
+		panel_desempate.add(comboBox_des2);
+		
+		label = new JLabel("");
+		panel_desempate.add(label);
+		
+		comboBox_des3 = new JComboBox();
+		comboBox_des3.setModel(new DefaultComboBoxModel(new String[] {"", "3\u00BA Progresivo", "3\u00BA Bucholz", "3\u00BA SonnebornBerger"}));
+		panel_desempate.add(comboBox_des3);
 		
 		panelboton = new JPanel();
 		panelboton.setBackground(new Color(0, 191, 255));
