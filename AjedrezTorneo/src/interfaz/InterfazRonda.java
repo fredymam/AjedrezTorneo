e5ef6fcb76a83c2cd0efa63253a789f681b1b647
Javier <javier.castanino@gmail.com>
2016-11-11 19:00:59 -0300

    Trabaje en Ronda e InterfazRonda

--- AjedrezTorneo/src/interfaz/InterfazRonda.java
+++ AjedrezTorneo/src/interfaz/InterfazRonda.java
@@ -1,22 +1,51 @@
 package interfaz;
 
+import logica.Ronda;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.DefaultComboBoxModel;
 
 public class InterfazRonda {
-
-	private JFrame frame;
+    private Ronda round;
+	private JFrame frmInfoRonda;
+	private JComboBox<String> NumRonda;
 
 	
-	public InterfazRonda() {
+	public InterfazRonda(Ronda ronda) {
+		round = ronda;
 		initialize();
+		frmInfoRonda.setVisible(true);
 	}
 
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmInfoRonda = new JFrame();
+		frmInfoRonda.setTitle("Informacion Ronda");
+		frmInfoRonda.setBounds(100, 100, 450, 300);
+		frmInfoRonda.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frmInfoRonda.getContentPane().setLayout(null);
+		
+		JLabel lblRondaN = new JLabel("Ronda N\u00BA:");
+		lblRondaN.setBounds(83, 46, 71, 14);
+		frmInfoRonda.getContentPane().add(lblRondaN);
+		
+		NumRonda = new JComboBox<String>();
+		NumRonda.setModel(new DefaultComboBoxModel(new String[] {"", "1"}));
+		NumRonda.setBounds(178, 43, 50, 20);
+		frmInfoRonda.getContentPane().add(NumRonda);
+		
+		JButton btnCargar = new JButton("Cargar");
+		btnCargar.setBounds(258, 42, 89, 23);
+		frmInfoRonda.getContentPane().add(btnCargar);
 	}
 
+	public JComboBox<String> getNumRonda() {
+		return NumRonda;
+	}
+
+	public void setNumRonda(JComboBox<String> numRonda) {
+		NumRonda = numRonda;
+	}
 }
