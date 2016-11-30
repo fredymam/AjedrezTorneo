82fbce310561cc73929c2b7713e23dc937de9f1e
milo54 <miligauna1@hotmail.com>
2016-11-30 20:20:51 -0300

    Interfaz TieBreak

--- AjedrezTorneo/src/interfaz/InterfazTieBreak.java
+++ AjedrezTorneo/src/interfaz/InterfazTieBreak.java
@@ -1,5 +1,6 @@
 package interfaz;
 
+import logica.TieBreak;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
@@ -27,46 +28,46 @@ public class InterfazTieBreak extends JFrame  {
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		getContentPane().setLayout(gridBagLayout);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(153, 204, 102));
-		panel_1.setForeground(Color.ORANGE);
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.anchor = GridBagConstraints.NORTH;
-		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_1.gridx = 0;
-		gbc_panel_1.gridy = 0;
-		getContentPane().add(panel_1, gbc_panel_1);
-		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{203, 28, 0};
-		gbl_panel_1.rowHeights = new int[] {50, 0};
-		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel_1.setLayout(gbl_panel_1);
+		JPanel panelEncabezado = new JPanel();
+		panelEncabezado.setBackground(new Color(153, 204, 102));
+		panelEncabezado.setForeground(Color.ORANGE);
+		GridBagConstraints gbc_panelEncabezado = new GridBagConstraints();
+		gbc_panelEncabezado.anchor = GridBagConstraints.NORTH;
+		gbc_panelEncabezado.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panelEncabezado.insets = new Insets(0, 0, 5, 0);
+		gbc_panelEncabezado.gridx = 0;
+		gbc_panelEncabezado.gridy = 0;
+		getContentPane().add(panelEncabezado, gbc_panelEncabezado);
+		GridBagLayout gbl_panelEncabezado = new GridBagLayout();
+		gbl_panelEncabezado.columnWidths = new int[]{203, 28, 0};
+		gbl_panelEncabezado.rowHeights = new int[] {50, 0};
+		gbl_panelEncabezado.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelEncabezado.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panelEncabezado.setLayout(gbl_panelEncabezado);
 		
-		JLabel lblTiebreakmetodosDe = new JLabel("TieBreak (Metodos de Desempates)");
-		lblTiebreakmetodosDe.setForeground(new Color(0, 0, 0));
-		lblTiebreakmetodosDe.setBackground(new Color(153, 204, 0));
-		lblTiebreakmetodosDe.setFont(new Font("Castellar", Font.PLAIN, 14));
-		GridBagConstraints gbc_lblTiebreakmetodosDe = new GridBagConstraints();
-		gbc_lblTiebreakmetodosDe.insets = new Insets(0, 0, 0, 5);
-		gbc_lblTiebreakmetodosDe.gridx = 0;
-		gbc_lblTiebreakmetodosDe.gridy = 0;
-		panel_1.add(lblTiebreakmetodosDe, gbc_lblTiebreakmetodosDe);
+		JLabel Encabezado = new JLabel("TieBreak (Metodos de Desempates)");
+		Encabezado.setForeground(new Color(0, 0, 0));
+		Encabezado.setBackground(new Color(153, 204, 0));
+		Encabezado.setFont(new Font("Castellar", Font.PLAIN, 14));
+		GridBagConstraints gbc_Encabezado = new GridBagConstraints();
+		gbc_Encabezado.insets = new Insets(0, 0, 0, 5);
+		gbc_Encabezado.gridx = 0;
+		gbc_Encabezado.gridy = 0;
+		panelEncabezado.add(Encabezado, gbc_Encabezado);
 		
-		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.insets = new Insets(0, 0, 5, 0);
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 1;
-		getContentPane().add(panel, gbc_panel);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{320, 109, 0};
-		gbl_panel.rowHeights = new int[]{20, 20, 20, 10, 0, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		JPanel panelCuerpo = new JPanel();
+		GridBagConstraints gbc_panelCuerpo = new GridBagConstraints();
+		gbc_panelCuerpo.fill = GridBagConstraints.BOTH;
+		gbc_panelCuerpo.insets = new Insets(0, 0, 5, 0);
+		gbc_panelCuerpo.gridx = 0;
+		gbc_panelCuerpo.gridy = 1;
+		getContentPane().add(panelCuerpo, gbc_panelCuerpo);
+		GridBagLayout gbl_panelCuerpo = new GridBagLayout();
+		gbl_panelCuerpo.columnWidths = new int[]{320, 109, 0};
+		gbl_panelCuerpo.rowHeights = new int[]{20, 20, 20, 10, 0, 0};
+		gbl_panelCuerpo.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelCuerpo.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panelCuerpo.setLayout(gbl_panelCuerpo);
 		
 		txtBucholz = new JTextField();
 		txtBucholz.setText("1.");
@@ -75,16 +76,16 @@ public class InterfazTieBreak extends JFrame  {
 		gbc_txtBucholz.insets = new Insets(0, 0, 5, 5);
 		gbc_txtBucholz.gridx = 0;
 		gbc_txtBucholz.gridy = 0;
-		panel.add(txtBucholz, gbc_txtBucholz);
+		panelCuerpo.add(txtBucholz, gbc_txtBucholz);
 		txtBucholz.setColumns(10);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Bucholz", "Progresivo", "SonnebornBerger"}));
-		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
-		gbc_comboBox_1.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox_1.gridx = 1;
-		gbc_comboBox_1.gridy = 0;
-		panel.add(comboBox_1, gbc_comboBox_1);
+		JComboBox comboTieBreak1 = new JComboBox();
+		comboTieBreak1.setModel(new DefaultComboBoxModel(new String[] {"Bucholz", "Progresivo", "SonnebornBerger"}));
+		GridBagConstraints gbc_comboTieBreak1 = new GridBagConstraints();
+		gbc_comboTieBreak1.insets = new Insets(0, 0, 5, 0);
+		gbc_comboTieBreak1.gridx = 1;
+		gbc_comboTieBreak1.gridy = 0;
+		panelCuerpo.add(comboTieBreak1, gbc_comboTieBreak1);
 		
 		txtProgresivo = new JTextField();
 		txtProgresivo.setText("2.");
@@ -93,17 +94,17 @@ public class InterfazTieBreak extends JFrame  {
 		gbc_txtProgresivo.insets = new Insets(0, 0, 5, 5);
 		gbc_txtProgresivo.gridx = 0;
 		gbc_txtProgresivo.gridy = 1;
-		panel.add(txtProgresivo, gbc_txtProgresivo);
+		panelCuerpo.add(txtProgresivo, gbc_txtProgresivo);
 		txtProgresivo.setColumns(10);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Bucholz", "Progresivo", "SonnebornBerger"}));
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.anchor = GridBagConstraints.NORTHWEST;
-		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox.gridx = 1;
-		gbc_comboBox.gridy = 1;
-		panel.add(comboBox, gbc_comboBox);
+		JComboBox comboTieBreak2 = new JComboBox();
+		comboTieBreak2.setModel(new DefaultComboBoxModel(new String[] {"Bucholz", "Progresivo", "SonnebornBerger"}));
+		GridBagConstraints gbc_comboTieBreak2 = new GridBagConstraints();
+		gbc_comboTieBreak2.anchor = GridBagConstraints.NORTHWEST;
+		gbc_comboTieBreak2.insets = new Insets(0, 0, 5, 0);
+		gbc_comboTieBreak2.gridx = 1;
+		gbc_comboTieBreak2.gridy = 1;
+		panelCuerpo.add(comboTieBreak2, gbc_comboTieBreak2);
 		
 		txtSonnebornberger = new JTextField();
 		txtSonnebornberger.setText("3.");
@@ -112,45 +113,45 @@ public class InterfazTieBreak extends JFrame  {
 		gbc_txtSonnebornberger.insets = new Insets(0, 0, 5, 5);
 		gbc_txtSonnebornberger.gridx = 0;
 		gbc_txtSonnebornberger.gridy = 2;
-		panel.add(txtSonnebornberger, gbc_txtSonnebornberger);
+		panelCuerpo.add(txtSonnebornberger, gbc_txtSonnebornberger);
 		txtSonnebornberger.setColumns(10);
 		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Bucholz", "Progresivo", "SonnebornBerger"}));
-		GridBagConstraints gbc_comboBox_2 = new GridBagConstraints();
-		gbc_comboBox_2.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox_2.gridx = 1;
-		gbc_comboBox_2.gridy = 2;
-		panel.add(comboBox_2, gbc_comboBox_2);
+		JComboBox comboTieBreak3 = new JComboBox();
+		comboTieBreak3.setModel(new DefaultComboBoxModel(new String[] {"Bucholz", "Progresivo", "SonnebornBerger"}));
+		GridBagConstraints gbc_comboTieBreak3 = new GridBagConstraints();
+		gbc_comboTieBreak3.insets = new Insets(0, 0, 5, 0);
+		gbc_comboTieBreak3.gridx = 1;
+		gbc_comboTieBreak3.gridy = 2;
+		panelCuerpo.add(comboTieBreak3, gbc_comboTieBreak3);
 		
-		JButton btnNewButton = new JButton("Guardar");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 0;
-		gbc_btnNewButton.gridy = 3;
-		panel.add(btnNewButton, gbc_btnNewButton);
+		JButton btnGuardar = new JButton("Guardar");
+		GridBagConstraints gbc_btnGuardar = new GridBagConstraints();
+		gbc_btnGuardar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnGuardar.gridx = 0;
+		gbc_btnGuardar.gridy = 3;
+		panelCuerpo.add(btnGuardar, gbc_btnGuardar);
 		
-		JPanel panel_2 = new JPanel();
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_2.anchor = GridBagConstraints.NORTH;
-		gbc_panel_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_panel_2.gridx = 0;
-		gbc_panel_2.gridy = 2;
-		getContentPane().add(panel_2, gbc_panel_2);
-		GridBagLayout gbl_panel_2 = new GridBagLayout();
-		gbl_panel_2.columnWidths = new int[]{153, 127, 0};
-		gbl_panel_2.rowHeights = new int[] {25, 0};
-		gbl_panel_2.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_2.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel_2.setLayout(gbl_panel_2);
+		JPanel panelRegistrar = new JPanel();
+		GridBagConstraints gbc_panelRegistrar = new GridBagConstraints();
+		gbc_panelRegistrar.insets = new Insets(0, 0, 5, 0);
+		gbc_panelRegistrar.anchor = GridBagConstraints.NORTH;
+		gbc_panelRegistrar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panelRegistrar.gridx = 0;
+		gbc_panelRegistrar.gridy = 2;
+		getContentPane().add(panelRegistrar, gbc_panelRegistrar);
+		GridBagLayout gbl_panelRegistrar = new GridBagLayout();
+		gbl_panelRegistrar.columnWidths = new int[]{153, 127, 0};
+		gbl_panelRegistrar.rowHeights = new int[] {25, 0};
+		gbl_panelRegistrar.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelRegistrar.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panelRegistrar.setLayout(gbl_panelRegistrar);
 		
 		JButton btnRegistrarFormulario = new JButton("Registrar formulario");
 		GridBagConstraints gbc_btnRegistrarFormulario = new GridBagConstraints();
 		gbc_btnRegistrarFormulario.anchor = GridBagConstraints.NORTHWEST;
 		gbc_btnRegistrarFormulario.gridx = 1;
 		gbc_btnRegistrarFormulario.gridy = 0;
-		panel_2.add(btnRegistrarFormulario, gbc_btnRegistrarFormulario);
+		panelRegistrar.add(btnRegistrarFormulario, gbc_btnRegistrarFormulario);
 	}
 
 }
