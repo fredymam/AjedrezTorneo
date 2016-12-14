73b2d82e0be50b85d72b62779a4ef4d41bbfca19
Maxi Zapata <zapatamaxi20@gmail.com>
2016-12-14 20:15:30 -0300

    Modifique el interfaz!!

--- AjedrezTorneo/src/interfaz/InterfazJugador.java
+++ AjedrezTorneo/src/interfaz/InterfazJugador.java
@@ -30,14 +30,13 @@ public class InterfazJugador {
 	private JTextField jtexfield_elo;
 	private JComboBox<String> comboBox_sexo;
 	private JTextField jtexfield_apellido;
-	private JComboBox<String> comboBox_titulo;
 	private Container panel_5; 
 	private JTextField textField_1;
 	private JComboBox<String> combodia; 
 	private JComboBox<String> combomes;
 	private JComboBox<String> comboanio;
 	private JLabel lblNewLabel_5 ;
-	private JComboBox combotitulo;
+	private JComboBox<?> combotitulo;
 	private JLabel lblNewLabel_correo; 
 	
 	
@@ -54,43 +53,34 @@ public class InterfazJugador {
 		frame.getContentPane().setBackground(new Color(154, 205, 50));
 		frame.setBounds(100, 100, 348, 357);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(9, 1, 0, 0));
-		
-		JLabel lblNewLabel = new JLabel("JUGADOR");
-		lblNewLabel.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
-		lblNewLabel.setForeground(new Color(0, 0, 0));
-		lblNewLabel.setBackground(SystemColor.textHighlight);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		frame.getContentPane().add(lblNewLabel);
-		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel);
+		frame.getContentPane().setLayout(new GridLayout(10, 2, 0, 0));
 		
 		JLabel lblNewLabel_1 = new JLabel("Nombre");
+		frame.getContentPane().add(lblNewLabel_1);
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
-		panel.add(lblNewLabel_1);
 		
 		jtexfield_nombre = new JTextField();
-		panel.add(jtexfield_nombre);
+		frame.getContentPane().add(jtexfield_nombre);
 		jtexfield_nombre.setColumns(10);
 		
 		JLabel lblApellido = new JLabel("Apellido");
+		frame.getContentPane().add(lblApellido);
 		lblApellido.setHorizontalAlignment(SwingConstants.CENTER);
 		lblApellido.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
-		panel.add(lblApellido);
 		
 		jtexfield_apellido = new JTextField();
-		panel.add(jtexfield_apellido);
+		frame.getContentPane().add(jtexfield_apellido);
 		jtexfield_apellido.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("F.Nac");
+		frame.getContentPane().add(lblNewLabel_2);
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
 		
 		JPanel panel_1 = new JPanel();
 		frame.getContentPane().add(panel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("F.Nac");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
-		panel_1.add(lblNewLabel_2);
+		panel_1.setLayout(new GridLayout(0, 3, 0, 0));
 		
 		combodia = new JComboBox<String>();
 		combodia.setModel(new DefaultComboBoxModel(new String[] {"DD", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
@@ -106,31 +96,53 @@ public class InterfazJugador {
             comboanio.addItem(String.valueOf(f));
         }
         add(comboanio);
-		panel_1.add(comboanio); 
+		panel_1.add(comboanio);
 		
-		JPanel panel_2 = new JPanel();
-		frame.getContentPane().add(panel_2);
+		lblNewLabel_correo = new JLabel("Correo:");
+		frame.getContentPane().add(lblNewLabel_correo);
+		lblNewLabel_correo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_correo.setFont(new Font("Bookman Old Style", Font.BOLD, 11));
+		
+		textField_1 = new JTextField();
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Sexo");
+		frame.getContentPane().add(lblNewLabel_3);
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_3.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
-		panel_2.add(lblNewLabel_3);
 		
 		comboBox_sexo = new JComboBox();
+		frame.getContentPane().add(comboBox_sexo);
 		comboBox_sexo.setModel(new DefaultComboBoxModel(new String[] {"M", "F", "Otro"}));
 		comboBox_sexo.setSelectedIndex(2);
-		panel_2.add(comboBox_sexo);
 		
-		JPanel panel_3 = new JPanel();
-		frame.getContentPane().add(panel_3);
+		JLabel lblTitulo = new JLabel("Titulo");
+		frame.getContentPane().add(lblTitulo);
+		lblTitulo.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		combotitulo = new JComboBox();
+		frame.getContentPane().add(combotitulo);
+		combotitulo.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		combotitulo.setModel(new DefaultComboBoxModel(new String[] {"", "GMI ", "GM", "MI", "MF ", "F"}));
+		
+		lblNewLabel_5 = new JLabel("Pais:");
+		frame.getContentPane().add(lblNewLabel_5);
+		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_5.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
+		
+		JComboBox combopais = new JComboBox(); //http://laendercode.net/es/3-letter-list.html
+		frame.getContentPane().add(combopais);
+		combopais.setModel(new DefaultComboBoxModel(new String[] {"", "AFG", "", "ALA", "", "ALB", "", "DZA", "", "ASM", "", "AND", "", "AGO", "", "AIA", "", "ATA", "", "ATG", "", "ARG", "", "ARM", "", "ABW", "", "AUS", "", "AUT", "", "AZE", "", "BHS", "", "BHR", "", "BGD", "", "BRB", "", "BLR", "", "BEL", "", "BLZ", "", "BEN", "", "BMU", "", "BTN", "", "BOL", "", "BES", "", "BIH", "", "BWA", "", "BVT", "", "BRA", "", "IOT", "", "VGB", "", "BRN", "", "BGR", "", "BFA", "", "BDI", "", "KHM", "", "CMR", "", "CAN", "", "CPV", "", "CYM", "", "CAF", "", "TCD", "", "CHL", "", "CHN", "", "CXR", "", "CCK", "", "COL", "", "COM", "", "COK", "", "CRI", "", "HRV", "", "CUB", "", "CUW", "", "CYP", "", "CZE", "", "COD", "", "DNK", "", "DJI", "", "DMA", "", "DOM", "", "TLS", "", "ECU", "", "EGY", "", "SLV", "", "GNQ", "", "ERI", "", "EST", "", "ETH", "", "FLK", "", "FRO", "", "FJI", "", "FIN", "", "FRA", "", "GUF", "", "PYF", "", "ATF", "", "GAB", "", "GMB", "", "GEO", "", "DEU", "", "GHA", "", "GIB", "", "GRC", "", "GRL", "", "GRD", "", "GLP", "", "GUM", "", "GTM", "", "GGY", "", "GIN", "", "GNB", "", "GUY", "", "HTI", "", "HMD", "", "HND", "", "HKG", "", "HUN", "", "ISL", "", "IND", "", "IDN", "", "IRN", "", "IRQ", "", "IRL", "", "IMN", "", "ISR", "", "ITA", "", "CIV", "", "JAM", "", "JPN", "", "JEY", "", "JOR", "", "KAZ", "", "KEN", "", "KIR", "", "XKX", "", "KWT", "", "KGZ", "", "LAO", "", "LVA", "", "LBN", "", "LSO", "", "LBR", "", "LBY", "", "LIE", "", "LTU", "", "LUX", "", "MAC", "", "MKD", "", "MDG", "", "MWI", "", "MYS", "", "MDV", "", "MLI", "", "MLT", "", "MHL", "", "MTQ", "", "MRT", "", "MUS", "", "MYT", "", "MEX", "", "FSM", "", "MDA", "", "MCO", "", "MNG", "", "MNE", "", "MSR", "", "MAR", "", "MOZ", "", "MMR", "", "NAM", "", "NRU", "", "NPL", "", "NLD", "", "ANT", "", "NCL", "", "NZL", "", "NIC", "", "NER", "", "NGA", "", "NIU", "", "NFK", "", "PRK", "", "MNP", "", "NOR", "", "OMN", "", "PAK", "", "PLW", "", "PSE", "", "PAN", "", "PNG", "", "PRY", "", "PER", "", "PHL", "", "PCN", "", "POL", "", "PRT", "", "PRI", "", "QAT", "", "COG", "", "REU", "", "ROU", "", "RUS", "", "RWA", "", "BLM", "", "SHN", "", "KNA", "", "LCA", "", "MAF", "", "SPM", "", "VCT", "", "WSM", "", "SMR", "", "STP", "", "SAU", "", "SEN", "", "SRB", "", "SCG", "", "SYC", "", "SLE", "", "SGP", "", "SXM", "", "SVK", "", "SVN", "", "SLB", "", "SOM", "", "ZAF", "", "SGS", "", "KOR", "", "SSD", "", "ESP", "", "LKA", "", "SDN", "n", "SUR", "", "SJM", "", "SWZ", "", "SWE", "", "CHE", "", "SYR", "", "TWN", "", "TJK", "", "TZA", "", "THA", "", "TGO", "", "TKL", "", "TON", "", "TTO", "", "TUN", "", "TUR", "", "TKM", "", "TCA", "", "TUV", "", "VIR", "", "UGA", "", "UKR", "", "ARE", "", "GBR", "", "USA", "", "UMI", "", "URY", "", "UZB", "", "VUT", "", "VAT", "", "VEN", "", "VNM", "", "WLF", "", "ESH", "", "YEM", "", "ZMB", "", "ZWE"}));
 		
 		JLabel lblNewLabel_4 = new JLabel("ELO:");
+		frame.getContentPane().add(lblNewLabel_4);
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_4.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
-		panel_3.add(lblNewLabel_4);
 		
 		jtexfield_elo = new JTextField();
-		panel_3.add(jtexfield_elo);
+		frame.getContentPane().add(jtexfield_elo);
 		jtexfield_elo.setColumns(10);
 		
 		
@@ -142,42 +154,8 @@ public class InterfazJugador {
 			}
 		});
 		
-		JPanel panel_4 = new JPanel();
-		frame.getContentPane().add(panel_4);
-		panel_4.setLayout(new GridLayout(0, 2, 0, 0));
-		
-		JLabel lblTitulo = new JLabel("Titulo");
-		lblTitulo.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
-		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_4.add(lblTitulo);
-		
-		combotitulo = new JComboBox();
-		combotitulo.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		combotitulo.setModel(new DefaultComboBoxModel(new String[] {"", "GMI ", "GM", "MI", "MF ", "F"}));
-		panel_4.add(combotitulo);
-		
-		JPanel panel_6 = new JPanel();
-		frame.getContentPane().add(panel_6);
-		
-		lblNewLabel_5 = new JLabel("Pais:");
-		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_5.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
-		panel_6.add(lblNewLabel_5);
-		
-		JComboBox combopais = new JComboBox(); //http://laendercode.net/es/3-letter-list.html
-		panel_6.add(combopais);
-		
-		JPanel panel_7 = new JPanel();
-		frame.getContentPane().add(panel_7);
-		
-		lblNewLabel_correo = new JLabel("Correo:");
-		lblNewLabel_correo.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_correo.setFont(new Font("Bookman Old Style", Font.BOLD, 11));
-		panel_7.add(lblNewLabel_correo);
-		
-		textField_1 = new JTextField();
-		panel_7.add(textField_1);
-		textField_1.setColumns(10);
+		JLabel label = new JLabel("");
+		frame.getContentPane().add(label);
 		frame.getContentPane().add(btnNewButton);
 		//frame.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{lblNewLabel, panel, lblNewLabel_1, jtexfield_nombre, lblApellido, jtexfield_apellido, panel_1, lblNewLabel_2, comboBox_1, comboBox_2, comboanio, panel_2, lblNewLabel_3, comboBox_sexo, panel_3, lblNewLabel_4, jtexfield_elo, btnNewButton}));
 	}
@@ -200,12 +178,11 @@ public class InterfazJugador {
 		
 		jugador.setNacimiento(combodia.getSelectedItem().toString()+"/"+combomes.getSelectedItem().toString()+"/"+comboanio.getSelectedItem().toString());
 		jugador.setElo(Integer.parseInt(jtexfield_elo.getText()));
-		jugador.setTitulo((String) comboBox_titulo.getSelectedItem());
+		jugador.setTitulo((String) combotitulo.getSelectedItem());
 		jugador.setCorreo(lblNewLabel_correo.getText());
 		jugador.setPais(lblNewLabel_5 .getText());
 		
 		
-		JOptionPane.showMessageDialog(null, "Jugador Registrado");
 	}
 	
 
